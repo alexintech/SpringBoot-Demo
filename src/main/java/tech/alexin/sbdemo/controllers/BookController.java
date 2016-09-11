83541697448fed7fdb330ad7c167869f67672412
Alexandr Cherepanov <xandr.cherepanov@gmail.com>
2016-09-11 18:50:47 +0600

    Deleting book entity

--- src/main/java/tech/alexin/sbdemo/controllers/BookController.java
+++ src/main/java/tech/alexin/sbdemo/controllers/BookController.java
@@ -3,8 +3,10 @@ package tech.alexin.sbdemo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import tech.alexin.sbdemo.repositories.BookRepository;
 
 @Controller
@@ -18,5 +20,11 @@ public class BookController {
     public String listBooks(Model model) {
         model.addAttribute("books", repository.findAll());
         return "books/list";
+    }
+
+    @RequestMapping(value = "/{id}/delete", method = RequestMethod.GET)
+    public ModelAndView delete(@PathVariable long id) {
+        repository.delete(id);
+        return new ModelAndView("redirect:/books");
     }
 }
